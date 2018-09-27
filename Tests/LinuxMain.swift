1ce431f9ad52e6961f81c86d08e66860e99c4ccb
Sergey Borovikov <sergey.borovikov@list.ru>
2018-09-27 21:05:45 +0500

    add LinuxMain file

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,10 @@
+
+//import XCTest
+//@testable import AppTests
+//
+//
+//XCTMain([
+//    testCase(AcronymTests.allTests),
+//    testCase(CategoryTests.allTests),
+//    testCase(UserTests.allTests)
+//    ])
